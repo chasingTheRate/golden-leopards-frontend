5fc249383113ec2024b9b78838ae4c26465e590c
Mark <mark.e.eaton@gmail.com>
2021-10-27 19:47:08 -0500

    updated url

--- src/api/goldenLeopardsApi.js
+++ src/api/goldenLeopardsApi.js
@@ -1,15 +1,15 @@
 
 export const getSeasons = async () => {
-  const results = await (await fetch(`https://golden-leopards.herokuapp.com/api/seasons`)).json();
+  const results = await (await fetch(`https://golden-leopards-backend.herokuapp.com/api/seasons`)).json();
   return results ? results : [];
 }
 
 export const getGames = async () => {
-  const results = await (await fetch(`https://golden-leopards.herokuapp.com/api/games`)).json();
+  const results = await (await fetch(`https://golden-leopards-backend.herokuapp.com/api/games`)).json();
   return results ? results : [];
 }
 
 export const getPlayers = async () => {
-  const results = await (await fetch(`https://golden-leopards.herokuapp.com/api/players`)).json();
+  const results = await (await fetch(`https://golden-leopards-backend.herokuapp.com/api/players`)).json();
   return results ? results : [];
 }
