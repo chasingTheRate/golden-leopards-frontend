f601c11271b900350ab1eda4e3ceeda30b060c06
Mark <mark.e.eaton@gmail.com>
2022-02-23 19:06:15 -0600

    filter tournaments

--- src/components/glHome.js
+++ src/components/glHome.js
@@ -7,18 +7,22 @@ import {
   Form
 } from 'react-bootstrap';
 import _ from 'lodash';
+import moment from 'moment';
 
-import backgroundVideo from '../assets/videos/background.mp4';
 import './glHome.css'
 import TournamentScheduleTable from '../components/tournamentScheduleTable';
 
 const GLHome = () => {
 
   const [tournamentSchedule, setTournamentSchedule] = useState([]);
+  const [filteredTournamentSchedule, setFilteredTournamentSchedule] = useState([]);
   const [roster, setRoster] = useState([]);
   const [show, setShow] = useState(false);
   const [selectedTournament, setSelectedTournament] = useState({});
- 
+  const [selectedMonth, setSelectedMonth] = useState();
+  const [selectedLocation, setSelectedLocation] = useState();
+
+
   useEffect(() => {
     const fetchData = async () => {
       await getSchedule();
@@ -31,6 +35,7 @@ const GLHome = () => {
   const getSchedule = async () => {
     const tournamentSchedule = await getTournamentSchedule();
     setTournamentSchedule(tournamentSchedule);
+    setFilteredTournamentSchedule(tournamentSchedule);
   }
 
   const handleClose = () => {
@@ -63,7 +68,6 @@ const GLHome = () => {
     const previousTournament = tournamentSchedule.find(t => t.id === selectedTournament.id);
 
     if (_.isEqual(previousTournament.players, selectedTournament.players)) {
-      console.log('test');
       handleClose();
       return;
     }
@@ -77,14 +81,103 @@ const GLHome = () => {
     }
   }
 
+  const handleMonthChange = (e) => {
+    e.preventDefault();
+    setSelectedMonth(e.target.value);
+    filterTournamentSchedule(null, e.target.value);
+  }
+
+  const handleLocationChange = (e) => {
+    e.preventDefault();
+    setSelectedLocation(e.target.value);
+    filterTournamentSchedule(e.target.value, null);
+  }
+
+  const isValidLocation = ({ isLocal = false, isOutOfState = false }, location) => {
+  
+    let isValid = false;
+
+    switch (location) {
+      case undefined:
+      case null:
+      case 'Location':
+      case 'all':
+        return true;
+      case 'local':
+        isValid = isLocal;
+        break;
+      case 'inState':
+        isValid = !isOutOfState
+        break;
+      case 'outOfState':
+        isValid = isOutOfState;
+        break;
+      default:
+        return true;
+    }
+
+    return isValid;
+  }
+
+  const isValidMonth = ({ ['Start Date']: startDate }, month) => {
+
+    return month ? moment(startDate, 'YYYY-MM-DD').format('MMM').toString() === month : true;
+  };
+
+  const filterTournamentSchedule = (location, month) => {
+    const defaultLocation = location ? location : selectedLocation;
+    const defaultMonth = month ? month : selectedMonth;
+
+    let filtered = tournamentSchedule.filter(t => isValidLocation(t, defaultLocation));
+    filtered = filtered.filter(t => isValidMonth(t, defaultMonth));
+    setFilteredTournamentSchedule(filtered);
+  }
+
   return (
     <Container fluid style={{ padding: 0, zIndex: 100}}>
       <Container style={{overflowX: 'scroll', padding: 0}}>
-        <div style={{color: 'rgb(100, 100, 100)', textAlign: 'center', padding: '0 0 0 3px', marginTop: '30px', marginBottom: '10px', fontWeight: 700 }}>
+        <div style={{color: 'rgb(100, 100, 100)', textAlign: 'center', padding: '0 0 0 3px', marginTop: '30px', marginBottom: '15px', fontWeight: 700 }}>
           <span>Upcoming Tournaments</span>
         </div>
+        <div style={{ margin: '15px 0 15px 0', textAlign: 'left' }}>
+          <span style={{
+                  fontSize: '0.7em',
+                  color: 'rgb(100 100 100)',
+                  fontWeight: 600
+                }}
+                >&nbsp; FILTER &nbsp;</span>
+          <div style={{display: 'flex' }}>
+            <div style={{margin: '0 8px 0 0', flexGrow: 1}}>
+              <Form.Select aria-label="Tournament Month" onChange={ handleMonthChange } value={ selectedMonth }>
+                <option>Month</option>
+                <option value="Jan">January</option>
+                <option value="Feb">February</option>
+                <option value="Mar">March</option>
+                <option value="Apr">April</option>
+                <option value="May">May</option>
+                <option value="Jun">June</option>
+                <option value="Jul">July</option>
+                <option value="Aug">August</option>
+                <option value="Sep">September</option>
+                <option value="Oct">October</option>
+                <option value="Nov">November</option>
+                <option value="Dec">December</option>
+              </Form.Select>
+            </div>
+          
+            <div style={{flexGrow: 1}}>
+              <Form.Select aria-label="Tournament Location" onChange={ handleLocationChange } value={ selectedLocation }>
+                <option>Location</option>
+                <option value="local">Local Only</option>
+                <option value="inState">Texas Only</option>
+                <option value="outOfState">Out of State</option>
+                <option value="all">All</option>
+              </Form.Select>
+            </div>
+          </div>
+        </div>
         <TournamentScheduleTable 
-          data={ tournamentSchedule }
+          data={ filteredTournamentSchedule }
           addPlayerToTournament= { addPlayerToTournament }
         ></TournamentScheduleTable>
       </Container>
