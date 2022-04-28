4d517a5e78fb798bc660a223fc04ce7c356a7c47
Mark <mark.e.eaton@gmail.com>
2022-04-28 15:43:01 -0500

    youtube test

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,20 @@ export async function getServerSideProps() {
 }
 
 const GLHome = ({ data }) => {
-
   return (
     <Container className="gl-home" fluid>
+      <div className='last-game-container' style={{ paddingTop: '30px'}}>
+        <div className='game-highlights-container' style={{ position: 'relative', width: '100%', paddingBottom: '56.25%'}}>
+          <iframe
+            style={{ position: 'absolute', top: 0,  left: 0, width: '100%', height: '100%', border: 0}}
+            src="https://www.youtube.com/embed/VW_zFouGEac?autoplay=1&mute=1"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+          </iframe>
+        </div>
+      </div>
       <div className="next-game-title-container">
         <h5>Next Games</h5>
       </div>
