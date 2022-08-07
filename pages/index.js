bcaf178e703cb155c5fd1e36b8784f641ad85272
Mark <mark.e.eaton@gmail.com>
2022-08-06 19:08:02 -0500

    memory leak

--- pages/index.js
+++ pages/index.js
@@ -9,20 +9,20 @@ import {
 import GLNextGameContainer from '../src/components/next-game/glNextGameContainer';
 import GLGameResultContainer from '../src/components/game-result/gameResultContainer';
 
-export async function getServerSideProps() {
-  const data = await getNextGames();
-  const lastGameResultsData = await getLastGameResults();
-  return { props: { data, lastGameResultsData } }
-}
+// export async function getServerSideProps() {
+//   const data = await getNextGames();
+//   const lastGameResultsData = await getLastGameResults();
+//   return { props: { data, lastGameResultsData } }
+// }
 
-const GLHome = ({ data, lastGameResultsData }) => {
+const GLHome = () => { //({ data, lastGameResultsData }) => {
 
   return (
     <Container className="gl-home" fluid>
       {/* <GLGameResultContainer data={ lastGameResultsData }></GLGameResultContainer> */}
-      { _.size(data) > 0 &&
+      {/* { _.size(data) > 0 &&
         <GLNextGameContainer data={ data }></GLNextGameContainer>
-      }
+      } */}
     </Container>
   );
 }
