9abd9cb31b31e2a6c0794f95c463f3d338ea36f7
Mark <mark.e.eaton@gmail.com>
2022-08-06 19:30:47 -0500

    bugs

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,18 @@ import {
 import GLNextGameContainer from '../src/components/next-game/glNextGameContainer';
 import GLGameResultContainer from '../src/components/game-result/gameResultContainer';
 
-// export async function getServerSideProps() {
-//   const data = await getNextGames();
-//   const lastGameResultsData = await getLastGameResults();
-//   return { props: { data, lastGameResultsData } }
-// }
+export async function getServerSideProps() {
+  const data = await getNextGames();
+  const lastGameResultsData = await getLastGameResults();
+  return { props: { data, lastGameResultsData } }
+}
 
-const GLHome = () => { //({ data, lastGameResultsData }) => {
+const GLHome = () => ({ data, lastGameResultsData }) => {
 
   return (
     <Container className="gl-home" fluid>
-      {/* <GLGameResultContainer data={ lastGameResultsData }></GLGameResultContainer> */}
-      {/* { _.size(data) > 0 &&
+      {/* <GLGameResultContainer data={ lastGameResultsData }></GLGameResultContainer>
+      { _.size(data) > 0 &&
         <GLNextGameContainer data={ data }></GLNextGameContainer>
       } */}
     </Container>
