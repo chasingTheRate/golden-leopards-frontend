295c4ec2ef45a9a9b795edb804287cee62ee347a
Mark <mark.e.eaton@gmail.com>
2023-06-16 11:13:55 -0500

    style

--- src/components/players/glPlayersListItem.js
+++ src/components/players/glPlayersListItem.js
@@ -13,9 +13,10 @@ const GLPlayerNumber = styled.div`
   justify-content: start;
   align-items: center;
   font-size: 25px;
-  width: 45px;
   color: #15469d;
-
+  flex-grow: 0;
+  flex-shrink: 0;
+  flex-basis: 35px;
   span {
     font-weight: 500
   }
@@ -25,7 +26,6 @@ const GLPlayerImage = styled.div`
   display: flex;
   justify-content: start;
   align-items: center;
-  width: 70px;
 `
 
 const GLPlayerImageMask = styled.div`
@@ -45,7 +45,6 @@ const GLPlayerName = styled.div`
   justify-content: center;
   align-items: center;
   font-size: 20px;
-  flex-grow: 1;
   color: #15469d;
 
   span {
@@ -54,12 +53,12 @@ const GLPlayerName = styled.div`
 `
 const GLPlayerStats = styled.div`
   display: flex;
+  flex-grow: 1;
   justify-content: ;center;
   align-items: start;
   font-size: 10px;
   color: rgb(75 75 75);
   flex-direction: column;
-  width: 70px;
 
   .gl-player-stats-title {
     font-weight: 700
@@ -70,36 +69,42 @@ const PlayerListItem = ({player }) => {
   return (
     <div>
       <div style={{display: 'flex'}}>
-        <GLPlayerNumber>
-          <span>{ player.jerseynumber }</span>
-        </GLPlayerNumber>
-        <GLPlayerImage>
-          { player.s3_filename 
-            ?
-              <GLPlayerImageMask>
-                <Image 
-                  src={ `https://d33nclgf902cx6.cloudfront.net/assets/players/jackie2.jpeg` } 
-                  alt="Logo" 
-                  height={ 60 } 
-                  width= { 60 }
-                />
-              </GLPlayerImageMask>
-            : 
-              <GLPlayerImageDefault>
-                <i className="bi bi-person-circle"></i>
-              </GLPlayerImageDefault>
-            }
-        </GLPlayerImage>
-        <GLPlayerName>
-          <span>{ player.displayname }</span>
-        </GLPlayerName>
-      <GLPlayerStats>
-          <div><span className="gl-player-stats-title">GAMES: </span><span>{player.games}</span></div>
-          <div><span className="gl-player-stats-title">GOALS: </span><span>{player.goals}</span></div>
-          <div><span className="gl-player-stats-title">ASSISTS: </span><span>{player.assists}</span></div>
-          <div><span className="gl-player-stats-title">TACKLES: </span><span>{player.tackles}</span></div>
-          <div><span className="gl-player-stats-title">SAVES: </span><span>{player.saves}</span></div>
-        </GLPlayerStats>
+        <div style={{display: 'flex', flexGrow: 1, flexBasis: 0}}>
+          <GLPlayerNumber>
+            <span>{ player.jerseynumber }</span>
+          </GLPlayerNumber>
+          <GLPlayerImage>
+            { player.s3_filename 
+              ?
+                <GLPlayerImageMask>
+                  <Image 
+                    src={ `https://d33nclgf902cx6.cloudfront.net/assets/players/jackie2.jpeg` } 
+                    alt="Logo" 
+                    height={ 60 } 
+                    width= { 60 }
+                  />
+                </GLPlayerImageMask>
+              : 
+                <GLPlayerImageDefault>
+                  <i className="bi bi-person-circle"></i>
+                </GLPlayerImageDefault>
+              }
+          </GLPlayerImage>
+        </div>
+        <div style={{display: 'flex', flexGrow: 2, flexBasis: 0, justifyContent: 'center', alignItems: 'center', minWidth: 0}}>
+          <GLPlayerName>
+            <span>{ player.displayname }</span>
+          </GLPlayerName>
+        </div>
+        <div style={{flexGrow: 1, flexBasis: 0}}>
+          <GLPlayerStats>
+            <div><span className="gl-player-stats-title">GAMES: </span><span>{player.games}</span></div>
+            <div><span className="gl-player-stats-title">GOALS: </span><span>{player.goals}</span></div>
+            <div><span className="gl-player-stats-title">ASSISTS: </span><span>{player.assists}</span></div>
+            <div><span className="gl-player-stats-title">TACKLES: </span><span>{player.tackles}</span></div>
+            <div><span className="gl-player-stats-title">SAVES: </span><span>{player.saves}</span></div>
+          </GLPlayerStats>
+        </div>
       </div>
       <hr></hr>
     </div>
